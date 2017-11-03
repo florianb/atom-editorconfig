3f3dbcc693d39286ee46bcb4d8d9aeafd53fcd99
Florian Breisch <florian.breisch@mindkeeper.solutions>
2017-11-03 18:51:09 +0100

    Update importLazy, fix first draft

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const importLazy = require('import-lazy')(require);
 
+const Editorconfig = importLazy('./lib/editorconfig');
 const generateConfig = importLazy('./commands/generate');
 const showState = importLazy('./commands/show');
 const fixFile = importLazy('./commands/fix');
 
-const atom = importLazy('atom');
-
 module.exports = {
 	activate(state) {
-		this.ecfgs = new WeakSet();
-		this.editors = new WeakMap();
-		
+		this.buffers = new WeakMap();
+
+		atom.workspace.observeTextEditors(editor => {
+			if (!editor) {
+				return;
+			}
+
+			const buffer = editor.getBuffer();
+			const ecfg = new Editorconfig(buffer);
+			ecfg.parseEditorconfig();
+
+			if (!this.buffers.has(buffer)) {
+				this.buffers.set(buffer, ecfg);
+			}
+		});
 	},
 	serialize() {},
 	deactivate() {}
 }
+
